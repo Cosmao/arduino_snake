612e67924e3bbeb033cec89014183b915a966567
Emil Kool <Emilhkool@gmail.com>
2023-10-19 16:10:10 +0200

    Added persistent movement

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -4,13 +4,18 @@
 TODO Add growing snake
 TODO Add apples to make it grow
 TODO Collision detection, go around other side?
-TODO Make it remember the direction and keep going if you dont change
 TODO make you lose somehow
  */
+
+#define up 0x00
+#define down 0x01
+#define right 0x02
+#define left 0x04
 
 //Joystick pins
 const int xAxisPin = 0;
 const int yAxisPin = 1;
+const int zAxisPin = 10;
 int limiter = 0;
 
 //Basic struct, I probably want to link them together and iterate util a nullPTR
@@ -20,7 +25,12 @@ struct snakePart{
     snakePart *ptr;
 };
 
-snakePart myHead;
+struct snakeHead : snakePart{
+    bool addPartOnMove;
+    uint8_t direction;
+};
+
+snakeHead myHead;
 
 //empty screenBuffer
 //column by column
@@ -33,48 +43,70 @@ byte screenBuffer[] = {
 
 void setup(){
     dotMatrix::setup();
+    pinMode(zAxisPin, INPUT_PULLUP);
+    Serial.begin(9600);
     myHead.x = 4;
     myHead.y = 4;
+    myHead.direction = 0x00;
     myHead.ptr = nullptr;
+    myHead.addPartOnMove = false;
 }
 
 void loop(){
     limiter++;
     dotMatrix::displayOnMatrix(screenBuffer);
+    setDirection(myHead);
     if(limiter % 50 == 0){
-        moveDot(myHead);
+        //moveDot(myHead);
+        move(myHead);
         updateScreenBuffer(myHead.x, myHead.y);
     }  
 }
 
-//Moves the dot according to joystick
-void moveDot(snakePart &ptr){
+//1024 max 0 min
+//512 avg
+//Sets the directions
+void setDirection(snakeHead &snake){
     int xVal, yVal;
     xVal = analogRead(xAxisPin);
     yVal = analogRead(yAxisPin);
+
     if(xVal < 100){
-        if(ptr.x == 7)
-            return;
-        ptr.x++;
-        return;
+        snake.direction = left;
     }
     if(xVal > 900){
-        if(ptr.x == 0)
-            return;
-        ptr.x--;
-        return;
+        snake.direction = right;
     }
     if(yVal < 100){
-        if(ptr.y == 0x01)
-            return;
-        ptr.y >>= 1;
-        return;
+        snake.direction = up;
     }
     if(yVal > 900){
-        if(ptr.y == 0x80)
-            return;
-        ptr.y <<= 1;
-        return;
+        snake.direction = down;
+    }
+}
+
+//moves depending on the direction set
+void move(snakeHead &snake){
+    switch(snake.direction){
+        case left:
+            if(snake.x != 7)
+                snake.x++;
+        break;
+
+        case right:
+            if(snake.x != 0)
+                snake.x--;
+        break;
+
+        case up:
+            if(snake.y !=0x01)
+                snake.y >>= 1;
+        break;
+        
+        case down:
+            if(snake.y !=0x80)
+                snake.y <<= 1;
+        break;
     }
 }
 
