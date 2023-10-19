ea8067e65358d6b6cb9ca94f981276214a077853
Emil Kool <Emilhkool@gmail.com>
2023-10-19 15:18:03 +0200

    Joystick support, now control 1 dot!

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -4,30 +4,85 @@
 Everything about the dotMatrix screen is handled by dotMatrix.h
 */
 
-uint8_t interval = 0;
+const int xAxisPin = 0;
+const int yAxisPin = 1;
 
-//screenbuffer already setup in an angle for the wave to properly work
+struct snakeHead{
+    uint8_t x;
+    uint8_t y;
+    snakeHead *ptr;
+};
+
+snakeHead myHead;
+
+//empty screenBuffer
+//column by column
 byte screenBuffer[] = {
-    0x80, 0x40,
-    0x20, 0x10,
-    0x8, 0x4,
-    0x2, 0x1
+    0x00, 0x00,
+    0x00, 0x00,
+    0x00, 0x00,
+    0x00, 0x00
 };
 
 void setup(){
     dotMatrix::setup();
+    myHead.x = 4;
+    myHead.y = 4;
 }
 
 void loop(){
-    interval ++;
     dotMatrix::displayOnMatrix(screenBuffer);
-    if(interval % 20 == 0)
-        wave();
+    moveDot(myHead);
+    updateScreenBuffer(myHead.x, myHead.y);
+}
+
+//Change to keeping track of direction
+void moveDot(snakeHead &ptr){
+    int xVal, yVal;
+    xVal = analogRead(xAxisPin);
+    yVal = analogRead(yAxisPin);
+    if(xVal < 100){
+        if(ptr.x == 7)
+            return;
+        ptr.x++;
+        return;
+    }
+    if(xVal > 900){
+        if(ptr.x == 0)
+            return;
+        ptr.x--;
+        return;
+    }
+    if(yVal < 100){
+        if(ptr.y == 0x01)
+            return;
+        ptr.y >>= 1;
+        return;
+    }
+    if(yVal > 900){
+        if(ptr.y == 0x80)
+            return;
+        ptr.y <<= 1;
+        return;
+    }
+}
+
+void updateScreenBuffer(uint8_t x, uint8_t y){
+    for(auto &i : screenBuffer){
+        i = 0x00;
+    }
+    screenBuffer[x] = y;
 }
 
 void wave(){
     for(auto &a : screenBuffer){
-        a <<= 1;
+        if((a & 0x80) == 0x80){
+            a <<= 1;
+            a++;
+        }
+        else{
+            a <<= 1;
+        }
         if(a == 0){
             a = 0x1;
         }
