d192b803035acc6ba055628ce7bccaea4ee0dc00
Emil Kool <Emilhkool@gmail.com>
2023-10-19 13:28:58 +0200

    Removed most dotmatrix code

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -1,7 +1,9 @@
+#include "dotMatrix.h"
+
 const int latchPin = 12;
 const int clockPin = 13;
 const int dataPin = 11;
-const int LEDPin[] = {2, 3, 4, 5, 6, 7, 8, 9};
+const uint8_t LEDPin[] = {2, 3, 4, 5, 6, 7, 8, 9};
 uint8_t interval = 0;
 
 byte screenBuffer[] = {
@@ -12,51 +14,21 @@ byte screenBuffer[] = {
 };
 
 void setup(){
-    pinMode(latchPin, OUTPUT);
-    pinMode(clockPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
-    for(int i = 0; i < 8; i++){
-        pinMode(LEDPin[i], OUTPUT);
-    }
+    dotMatrix::setup(latchPin, clockPin, dataPin, LEDPin);
 }
 
 void loop(){
     interval ++;
-    displayOnMatrix(screenBuffer);
-    if(interval % 50 == 0)
+    dotMatrix::displayOnMatrix(screenBuffer, LEDPin, latchPin, clockPin, dataPin);
+    if(interval % 20 == 0)
         wave();
 }
 
 void wave(){
     for(auto &a : screenBuffer){
         a <<= 1;
-        if(a == NULL){
+        if(a == 0){
             a = 0x1;
         }
     }
-}
-
-void displayOnMatrix(byte screenBuffer[]){
-    int cols = 0x01;
-    for(int i = 0; i < 8; i++){
-        matrixCol(cols);
-        matrixRows(screenBuffer[i]);
-        delay(1);
-        matrixRows(0x00); //Null terminated?
-        cols <<= 1;
-    }
-}
-
-void matrixCol(byte value){
-    byte cols = 0x01;
-    for(int i = 0; i < 8; i++){
-        digitalWrite(LEDPin[i], ((value & cols) == cols) ? LOW : HIGH);
-        cols <<= 1;
-    }
-}
-
-void matrixRows(byte value){
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, value);
-    digitalWrite(latchPin, HIGH);
 }
