014a418737da4be5945e423ebd20b30696a651a3
Emil Kool <Emilhkool@gmail.com>
2023-10-20 15:05:20 +0200

    Refactored code and lots of comments

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -7,15 +7,17 @@ TODO make you lose somehow
 TODO use the cleanup function to properly free the tail
  */
 
+//Some defines for clearer code
 #define up 0x00
 #define down 0x01
 #define right 0x02
 #define left 0x04
+#define gameSpeed 50
 
 //Joystick pins
 const int xAxisPin = 0;
 const int yAxisPin = 1;
-const int zAxisPin = 10;
+const int zAxisPin = 10;    //TODO remove once apples are here
 int limiter = 0;
 
 //Basic struct, I probably want to link them together and iterate util a nullPTR
@@ -25,12 +27,14 @@ struct snakePart{
     snakePart *nextPartPtr;
 };
 
+//extend the base struct with some extra features needed for head
 struct snakeHead : snakePart{
     bool addPartOnMove;
     uint8_t direction;
     snakePart *endPartPtr;
 };
 
+//Our "player"
 snakeHead myHead;
 
 //empty screenBuffer
@@ -44,50 +48,46 @@ byte screenBuffer[] = {
 
 void setup(){
     dotMatrix::setup();
-    pinMode(zAxisPin, INPUT_PULLUP);
+    pinMode(zAxisPin, INPUT_PULLUP);    //TODO remove once apples are here
     Serial.begin(9600);
-    myHead.direction = 0x00;
-    myHead.x = 4;
-    myHead.y = 4;
-    myHead.addPartOnMove = false;
-    myHead.endPartPtr = nullptr;
-    myHead.nextPartPtr = nullptr;
+    cleanUp(myHead);                    //just running the cleanup func to init to remove code duplication
 }
 
 void loop(){
     limiter++;
-    dotMatrix::displayOnMatrix(screenBuffer);
-    setDirection(myHead);
-    if(limiter % 50 == 0){
-        move(myHead);
-        updateScreenBuffer(myHead);
+    dotMatrix::displayOnMatrix(screenBuffer);   //Update the screen needs to run every loop otherwise it flickers
+    setDirection(myHead);                       //Gets the joystick direction
+    if(limiter % gameSpeed == 0){               //every gameSpeed actually do the logic
+        move(myHead);                           //moves the snake
+        updateScreenBuffer(myHead);             //updates the screenbuffer
     }  
 }
 
 //1024 max 0 min
 //512 avg
 //Sets the directions
+//TODO another pass on this function, kinda dirty
 void setDirection(snakeHead &snake){
-    int xVal, yVal, zVal;
+    int xVal, yVal, zVal;           //TODO remove zVal once apples are here
     xVal = analogRead(xAxisPin);
     yVal = analogRead(yAxisPin);
-    zVal = digitalRead(zAxisPin);
+    zVal = digitalRead(zAxisPin);   //TODO remove once apples are here
 
-    if(zVal == 0)
+    if(zVal == 0)                   //TODO remove once apples are here
         snake.addPartOnMove = true;
 
-    if(xVal < 100){
+    if(xVal < 100)
         snake.direction = left;
-    }
-    if(xVal > 900){
+
+    if(xVal > 900)
         snake.direction = right;
-    }
-    if(yVal < 100){
+    
+    if(yVal < 100)
         snake.direction = up;
-    }
-    if(yVal > 900){
+    
+    if(yVal > 900)
         snake.direction = down;
-    }
+    
 }
 
 //moves depending on the direction set
@@ -130,6 +130,7 @@ void move(snakeHead &snake){
     snake.addPartOnMove = false;
 }
 
+//TODO implement, run after moving head
 void collisionDetect(snakeHead &snake){
 
 }
@@ -139,12 +140,13 @@ void cleanUp(snakeHead &snake){
     clearSnakeTail(snake);
     snake.x = 4;
     snake.y = 4;
-    snake.direction = 0x00;
+    snake.direction = down;
     snake.nextPartPtr = nullptr;
     snake.endPartPtr = nullptr;
     snake.addPartOnMove = false;
 }
 
+//moves across the pointers to move all snake pieces
 void moveSnakePieces(snakePart &snakePiece, uint8_t x, uint8_t y){
     snakePart *snakePartPtr = snakePiece.nextPartPtr; //get next part
     if(snakePartPtr != nullptr){                     //we arent the last part
@@ -155,52 +157,52 @@ void moveSnakePieces(snakePart &snakePiece, uint8_t x, uint8_t y){
         moveSnakePieces(*snakePartPtr, curX, curY); //go again
     }
     else{
-        if(myHead.addPartOnMove) //if we just added one leave it where it is
-            return;
-        else{
+        if(!myHead.addPartOnMove){ //if we just added one leave it where it is
             snakePiece.x = x;
             snakePiece.y = y;
         }
     }
 }
 
-//Cleans screenbuffer and adds the lone dot back to it
+//updates the screenBuffer with the position of everything
 void updateScreenBuffer(snakeHead &snake){
     for(auto &i : screenBuffer){ //clean the screenBuffer
         i = 0x00;
     }
-    screenBuffer[snake.x] = snake.y; //Update column to the head
+    screenBuffer[snake.x] = snake.y; //Insert head, simple equal works since its first added
 
-    snakePart *snakePartPtr = snake.nextPartPtr;
-    while(snakePartPtr != nullptr){ //end at nullptr, otherwise iterate over all pieces
-        screenBuffer[snakePartPtr->x] = screenBuffer[snakePartPtr->x] | snakePartPtr->y; //bitwise OR
-        snakePartPtr = snakePartPtr->nextPartPtr;     //go to next part
+    snakePart *snakePartPtr = snake.nextPartPtr;        //get ptr for first part
+    while(snakePartPtr != nullptr){                     //end at nullptr, otherwise iterate over all pieces
+        screenBuffer[snakePartPtr->x] = screenBuffer[snakePartPtr->x] | snakePartPtr->y; //bitwise OR so we can handle several pieces on a column
+        snakePartPtr = snakePartPtr->nextPartPtr;       //set the ptr to next part
     }
 }
 
 void addPart(snakeHead &snake){
-    if(snake.nextPartPtr == nullptr){
-        snakePart* snakePiece = (snakePart*) malloc(sizeof(snakePart)); //malloc returns a void * so we need to cast to our struct
-        if(snakePiece == nullptr){      //malloc returns a nullptr if no space to allocate
-            Serial.println(F("MEM FULL")); //F() turns it into a string literal
+    if(snake.nextPartPtr == nullptr){       //Only runs as first piece is created
+        snakePart* snakePiece = (snakePart*) malloc(sizeof(snakePart)); //malloc returns a void * so we need to cast it to a ptr for our struct
+        if(snakePiece == nullptr){          //malloc returns a nullptr if no space to allocate
+            Serial.println(F("MEM FULL"));  //F() turns it into a string literal
+            cleanUp(snake);                 //malloc failed so cleanup the game (Should never happen)
         }
         else{       //update pointers and take the heads position, wont move last dot on the tick its created
             snake.nextPartPtr = snakePiece;
             snake.endPartPtr = snakePiece;
             snakePiece->x = snake.x;
             snakePiece->y = snake.y;
-            snakePiece->nextPartPtr = nullptr;
+            snakePiece->nextPartPtr = nullptr;              //making sure its a nullptr for other logic
         }
     }
-    else{
+    else{       //runs for all other pieces created
         snakePart* snakePiece = (snakePart*) malloc(sizeof(snakePart));
-        if(snakePiece == nullptr){      //malloc returns a nullptr if no space to allocate
+        if(snakePiece == nullptr){          //malloc returns a nullptr if no space to allocate
             Serial.println(F("MEM FULL"));
+            cleanUp(snake);                 //malloc failed so we reset game (Should never happen)
         }
         else{
-            snakePiece->x = snake.endPartPtr->x;             //Take the positions of the last element
+            snakePiece->x = snake.endPartPtr->x;            //Take the positions of the last element
             snakePiece->y = snake.endPartPtr->y;
-            snakePiece->nextPartPtr = nullptr;
+            snakePiece->nextPartPtr = nullptr;              //set the next piece as a nullptr since we are last
             snake.endPartPtr->nextPartPtr = snakePiece;     //endpointer pointing towards last piece set that pointer to the new piece
             snake.endPartPtr = snakePiece;                  //set as new last piece
         }
